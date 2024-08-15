6cbb122af13e06e36facafa2043df84955f614ad
krill <krill@krill-pc.net>
2024-08-15 15:33:26 -0600

    it works now

--- bike_lock.py
+++ bike_lock.py
@@ -1,4 +1,18 @@
-def main():
+def list_from_csv(path_to_file):
+    file = open(path_to_file, 'r')
+    contents = file.read()    #puts the contents of the file into a big string
+    contents_list = contents.split("\n")
+    return contents_list
+
+def trim_list(wordlist, number_of_letters):
+    wordlist2 = []
+    for i in range(len(wordlist)):
+        if len(wordlist[i]) == number_of_letters:
+               wordlist2.append(wordlist[i])
+
+    return wordlist2
+                            
+def get_dial_combinations():
     dials =[ ['s','p','h','m','t','w','d','l','f','b'],
              ['l','e','y','h','n','r','u','o','a','i'],
              ['e','n','m','l','r','t','a','o','s','k'],
@@ -13,8 +27,28 @@ def main():
                     str = dials[0][i] + dials[1][j] + dials[2][k] + dials[3][l]
                     combinations.append(str)
 
-                    
-    for i in range(len(combinations)):
-        print(combinations[i])
 
+    return combinations
+
+
+def main():
+    # dial_combinations = get_dial_combinations()
+    # for i in range(len(dial_combinations)):
+    #     print(dial_combinations[i])
+
+    four_letter_words_list = trim_list(list_from_csv("wordlist"), 4)
+    four_letter_words = {}
+    combinations = get_dial_combinations()
+
+    for i in range(len(four_letter_words_list)):
+        four_letter_words.update({four_letter_words_list[i]: i})
+
+    output = open("output.txt", "w")
+    for i in range(len(combinations)):
+        if combinations[i] in four_letter_words:
+            output.write(combinations[i])
+            output.write("\n")
+            output.flush()
+    output.close
+    
 main()
